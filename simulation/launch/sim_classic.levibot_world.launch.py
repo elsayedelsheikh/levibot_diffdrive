4d52c3f72859717c6fcf39cf4d082b870cf7b3ec
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2023-11-11 14:38:57 +0200

    ...

---
+++ simulation/launch/sim_classic.levibot_world.launch.py
@@ -0,0 +1,196 @@
+# $LICENSE$
+from os import environ
+from ament_index_python.packages import get_package_prefix
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+    TimerAction,
+)
+from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "runtime_config_package",
+            default_value="levibot_skidsteer",
+            description='Package with the controller\'s configuration in "config" folder. \
+        Usually the argument is not set, it enables use of a custom setup.',
+        )
+    )    
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="gz_controllers.yaml",
+            description="YAML file with the controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="levibot_skidsteer",
+            description="Description package with robot URDF/xacro files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="levibot.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_controller",
+            default_value="base_controller",
+            choices=["base_controller"],
+            description="Robot controller to start.",
+        )
+    )
+
+    # Initialize Arguments
+    runtime_config_package = LaunchConfiguration("runtime_config_package")
+    controllers_file = LaunchConfiguration("controllers_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    robot_controller = LaunchConfiguration("robot_controller")
+
+
+    robot_controllers = PathJoinSubstitution(
+        [FindPackageShare(runtime_config_package), "config", controllers_file]
+    )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "levibot.rviz"]
+    )
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "urdf", description_file]
+            ),
+            " ",
+            "use_mock_hardware:=false",
+            " ",
+            "sim_gazebo_classic:=true",
+            " ",
+            "sim_gazebo:=false",
+            " ",
+            "simulation_controllers:=",
+            robot_controllers,
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    # Gazebo nodes
+    # if '$GAZEBO_MODEL_PATH' in environ:
+    #     environ['$GAZEBO_MODEL_PATH'] =  environ['$GAZEBO_MODEL_PATH'] + ':' + get_package_prefix('levibot_skidsteer') + '/share/levibot_skidsteer/worlds'
+    # else:
+    #     environ['$GAZEBO_MODEL_PATH'] =  get_package_prefix('levibot_skidsteer') + "/share/levibot_skidsteer/worlds"
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("gazebo_ros"), "/launch", "/gazebo.launch.py"]
+        ),
+        launch_arguments={
+            "world": get_package_prefix('levibot_skidsteer') + "/share/levibot_skidsteer/worlds/levibot_world_classic.world",
+        }.items(),
+    )
+
+    # Spawn robot
+    gazebo_spawn_robot = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        name="spawn_levibot",
+        arguments=["-entity", "levibot", "-topic", "robot_description", "-x", "0.0", "-y", "0.0", "-z", "3.0"],
+        # arguments=["-entity", "levibot", "-topic", "robot_description", "-x", "13.9", "-y", "-10.6", "-z", "3.0"],
+        output="screen",
+    )
+
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controllers = [robot_controller]
+    robot_controller_spawners = []
+    for controller in robot_controllers:
+        robot_controller_spawners += [
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[controller, "-c", "/controller_manager"],
+            )
+        ]
+
+    # Delay loading and activation of `joint_state_broadcaster` after start of ros2_control_node
+    delay_joint_state_broadcaster_spawner_after_gazebo_spawn_robot = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=gazebo_spawn_robot,
+            on_exit=[joint_state_broadcaster_spawner],
+        )
+    )
+
+    # Delay rviz start after Joint State Broadcaster to avoid unnecessary warning output.
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
+
+    # Delay loading and activation of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawners_after_joint_state_broadcaster_spawner = []
+    for controller in robot_controller_spawners:
+        delay_robot_controller_spawners_after_joint_state_broadcaster_spawner += [
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=joint_state_broadcaster_spawner,
+                    on_exit=[
+                        TimerAction(
+                            period=3.0,
+                            actions=[controller],
+                        ),
+                    ],
+                )
+            )
+        ]
+
+    return LaunchDescription(
+        declared_arguments
+        + [
+            gazebo,
+            gazebo_spawn_robot,
+            robot_state_pub_node,
+            delay_rviz_after_joint_state_broadcaster_spawner,
+            delay_joint_state_broadcaster_spawner_after_gazebo_spawn_robot,
+        ]
+        + delay_robot_controller_spawners_after_joint_state_broadcaster_spawner
+    )
